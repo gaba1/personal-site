6201e130fd37ca2d3b627ea7cebf90ef903a936e
gaba1 <bruxblank@gmail.com>
2020-01-06 16:10:14 +0100

    Added

---
+++ skills.php
@@ -0,0 +1,70 @@
+<?php include "inc/head.php"; ?>
+
+<header class="header-skills">
+<div class="grid grid-2-1 container home-welcome">
+    
+    <div>
+      <h2 class="welcome">My skills & experiences</h2>
+    </div>
+
+    <div class="read-more">
+      <a href="#skills" class="btn">Read More</a>
+    </div>
+
+</div>
+<div class="bg-overlay"></div>
+  
+  </header>
+
+  
+<div id="skills" class="container skills">
+
+  <section class="grid">
+    <div>
+      <h2>Technologies</h2>
+    </div>
+    <div class="skills-list">
+      <div class="skills-1">
+        <h3>Day-to-day</h3>
+        <ul>
+          <li>HTML5 / CSS3</li>
+          <li>JavaScript</li>
+          <li>Version Control (GIT)</li>
+          <li>mySQL</li>
+          <li>Responsive Layout</li>
+          <li>Photoshop</li>
+        </ul>
+      </div>
+      <div class="skills-2">
+        <h3>Experience with</h3>
+        <ul>
+          <li>jQuery</li>
+          <li>PHP</li>
+          <li>Bootstrap</li>
+          <li>Materialize</li>
+          <li>impress.js</li>
+          <li>Adobe Illustrator</li>
+          <li>Cross-Browser Compatibility</li>
+          <li>Mobile First Thinking</li>
+          <li>Scrum</li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <div class="divider"></div>
+  <section class="grid">
+    <div>
+      <h2>What I want furthermore...</h2>
+    </div>
+    <div class="plans">
+     <p>Let me talk about my plans, what I would like to learn furthermore.</p>
+     <p>First of all, as I want to become a Frontend developer, I would like to improve my Frontend skills to a higher level. And besides, I learn some Backend techniques for that case to have an opportunity to become a Full Stack developer later</p>
+    </div>
+  </section>
+</div>
+
+<?php include "inc/footer.php"; ?>
+  </body>
+  <script src=".dist/js/app.js"></script>
+ 
+  </html>
